0d4b3050ca504a338c8b67b2eac693913d375850
Hector Toro <alejotoro94@hotmail.com>
2022-02-07 21:42:32 -0500

    Rutas hijas

--- src/app/components/usuario/usuario.component.ts
+++ src/app/components/usuario/usuario.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
-  styleUrls: ['./usuario.component.css']
+  styleUrls: []
 })
 export class UsuarioComponent implements OnInit {
 
