3ca447d01d9a980a63f842bb076b2c4ca933980f
Hector Toro <alejotoro94@hotmail.com>
2022-02-07 21:24:50 -0500

    Rutas y Rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { UsuarioComponent } from './components/usuario/usuario.component'
 
-const routes: Routes = [];
+const APP_ROUTES: Routes = [
+    { path: 'home', component: HomeComponent},
+    { path: 'usuario', component: UsuarioComponent},
+    { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
